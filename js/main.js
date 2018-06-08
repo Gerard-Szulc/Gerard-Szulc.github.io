2840f69eba49866d44bc724fdab39d152f98cb00
Gerard-Szulc <gerszulc05@gmail.com>
2018-06-08 02:11:54 +0200

    fixed button visibility and added wheel scrolling only to 2 sections. more come later

--- js/main.js
+++ js/main.js
@@ -12,14 +12,22 @@ arr.forEach(()=>{
     var nbsp = document.createElement('span');
 button.appendChild(nbsp)});
 
-topButtonContructor.addEventListener('click',event=>{event.preventDefault(); scroll(0,0); console.log('done')})
+topButtonContructor.addEventListener('click',event=>{
+    event.preventDefault(); scroll(0,0); console.log('done')
+})
 
 var body = document.getElementsByTagName('body')[0]
 var aboutLi = document.getElementById('aboutLi');
 var about = document.getElementById('about');
 var scrolled;
-const aboutLiHandler = event=> {event.preventDefault(); scroll(0,about.offsetTop);}
 
+const aboutLiHandler = event=> {
+    event.preventDefault(); scroll(0,about.offsetTop);
+}
+
+window.addEventListener('wheel',event=>{
+    event.preventDefault(); event.deltaY >0 ? scroll(0,about.offsetTop) : scroll(0,0)
+})
 aboutLi.addEventListener('click',aboutLiHandler)
 
 body.appendChild(topButtonContructor);
@@ -27,7 +35,7 @@ body.appendChild(topButtonContructor);
 setInterval(()=>{
     scrolled = window.scrollY
     scrolled > 0  && topButtonContructor.setAttribute('style','opacity: 0.8; visibility: visible;')
-    scrolled === 0 && topButtonContructor.setAttribute('style','opacity: 0;') && setTimeout(()=>topButtonContructor.setAttribute('visibility: hidden;'),250)
+    scrolled === 0 && topButtonContructor.setAttribute('style','opacity: 0; visibility: hidden')
 
   },500)
 
@@ -43,5 +51,10 @@ burger.addEventListener('mouseenter',
 burger.addEventListener('mouseleave',()=>{
     setTimeout(()=>burger.className = "",1000)
   })
+const navbar = document.getElementById('navbar')
+
+const navbarDriver = document.getElementById('show-menu')
+
+navbar.addEventListener('blur',()=>navbarDriver.checked = true)
   
 
